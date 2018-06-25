8a056ce440c45636f9bd138f824be891bb96fb80
OlgaM88 <olga.martynenko0102@gmail.com>
2018-06-25 14:24:44 +0300

    second commit

--- modul_2/index.js
+++ modul_2/index.js
@@ -1,18 +1,19 @@
 let numbers = [];
 let total = 0;
+let userInput;
 do {
-    let userInput = prompt('Enter number');
+    userInput = prompt('Enter number');
     numbers.push(Number(userInput));
     if(userInput === null || userInput === " "){
     numbers.pop();
     break;
     }
-} while(true);
+} while(userInput !== null);
 
 for (let i of numbers){
     total += i;
 } 
- if (numbers.length > 1){
+ if (numbers.length){
         const message = `Общая сумма чисел равна ${total}`;
         alert(message);
 }
