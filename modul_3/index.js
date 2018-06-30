b62d7a3eccc1a528f1768bf254151af053d8a4c5
OlgaM88 <olga.martynenko0102@gmail.com>
2018-06-30 17:36:37 +0300

    second commit

--- modul_3/index.js
+++ modul_3/index.js
@@ -1,32 +1,25 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 function checkLoginValidity(login) {
-  if (login.length > 4 && login.length < 16){
-    return true;
-  }return false;
+  return login.length >= 4 && login.length <= 16
 }
-console.log(checkLoginValidity('njkl;'));
 
 let checkIfLoginExists = function (logins, login) {
-if (logins.includes(login)){
-  return true;
-}return false;
+return logins.includes(login)
 }
-console.log(checkIfLoginExists(logins, "gfhgkhj,gj"));
 
-let addLogin = function (logins, login) {
+let addLogin = function() {
   let userLogin  = prompt("Введите свой логин");
 if (!checkLoginValidity(userLogin)){
-return console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+return alert('Ошибка! Логин должен быть от 4 до 16 символов');
 } else {
   if (!checkIfLoginExists(logins, userLogin)){
     logins.push(userLogin);
-    return 
-    console.log('Логин успешно добавлен!');
-  }else {
-    return 
-    console.log('Такой логин уже используется!');
+    return alert ('Логин успешно добавлен!');
+  }else if (checkIfLoginExists(logins, userLogin)) {
+    return alert ('Такой логин уже используется!');
   }
 }
 }
-console.log(addLogin(logins, 'Mango'));
+addLogin();
+console.log(logins);
